4eedbf67826f086654e318eec73c84eb8a61e428
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-08-11 23:37:37 -0500

    mvp

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function randomColor() {
     }
     return color;
 }
-//      click
+// 1      click
 const pTags = document.querySelectorAll('p');
 pTags.forEach(pTags => {
     pTags.addEventListener('click', event => {
@@ -17,86 +17,122 @@ pTags.forEach(pTags => {
         pTags.style.color = randomColor();
     })
 })
-
-//      Mouseover
+// 2    dblclick
+const funBus = document.querySelector('h1');
+funBus.addEventListener('dblclick', event => {
+    event.preventDefault();
+    funBus.textContent = 'Magic School Bus'
+    event.target.style.color = 'yellow';
+})
+// 3     Mouseover
 const aTags = document.querySelectorAll('a');
 aTags.forEach((aTags, index ) => {
     aTags.addEventListener('mouseover', event => {
         event.preventDefault();
         aTags.style.fontSize = '2rem';
     });
-//      Mouseout
+// 4     Mouseout
     aTags.addEventListener('mouseout', event => {
         event.preventDefault();
         aTags.style.fontSize = '1.6rem';
     });
 });
+// 5     Wheel
 
-//      Wheel
 let scroll = document.querySelector('.main-navigation');
 scroll.addEventListener('wheel', event => {
-  event.target.style.backgroundColor = 'yellow';
+  event.target.style.backgroundColor = randomColor();
 })
-//      Keydown
+// 6     Keydown
+
 const outputBox = document.createElement('p');
 document.querySelector('body').append(outputBox);
 document.addEventListener('keydown', event => {
     outputBox.textContent += `${event.key}`
 })
+// 7     Drag and Drop all p tags on page
 
-//      Drag and Drop
-var dragged;
-
+let dragged;
+pTags.forEach(pTag => {
+    pTag.draggable = true;
+    pTag.classList.add('dropzone')
+})
 /* events fired on the draggable target */
-pTags.addEventListener("drag", function(event) {}, false);
-
+document.addEventListener("drag", function(event) {}, false);
 document.addEventListener("dragstart", function(event) {
   // store a ref. on the dragged elem
   dragged = event.target;
   // make it half transparent
   event.target.style.opacity = .5;
-}, false);
-
+});
 document.addEventListener("dragend", function(event) {
   // reset the transparency
   event.target.style.opacity = "";
-}, false);
-
+});
 /* events fired on the drop targets */
 document.addEventListener("dragover", function(event) {
   // prevent default to allow drop
   event.preventDefault();
-}, false);
-
-pTags.addEventListener("dragenter", function(event) {
+});
+document.addEventListener("dragenter", function(event) {
   // highlight potential drop target when the draggable element enters it
   if (event.target.className == "dropzone") {
     event.target.style.background = "lightBlue";
   }
-
-}, false);
-
+});
 document.addEventListener("dragleave", function(event) {
   // reset background of potential drop target when the draggable element leaves it
   if (event.target.className == "dropzone") {
     event.target.style.background = "";
   }
-
-}, false);
-
+});
 document.addEventListener("drop", function(event) {
   // prevent default action (open as link for some elements)
   event.preventDefault();
   // move dragged elem to the selected drop target
   if (event.target.className == "dropzone") {
     event.target.style.background = "";
-    dragged.parentNode.removeChild( dragged );
-    event.target.appendChild( dragged );
+    let temp = dragged.textContent 
+    dragged.textContent = event.target.textContent;
+    event.target.textContent = temp;
   }
-}, false);
+});
 
+// 8     Focus
+const password = document.querySelector('input[type="password"]');
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';    
+});
 
+// 9     Blur
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+});
 
+// 10    Scroll
+
+const pics = document.querySelectorAll('img');
+let blur = 0;
+let lastKnownPosition = 0;
+let ticking = false;
+
+window.addEventListener('scroll', event => {
+    if(lastKnownPosition < window.scrollY){
+        blur += 1
+    }
+    else if(lastKnownPosition > window.scrollY){
+        blur -= 1
+    }
+    lastKnownPosition = window.scrollY;
+    pics.forEach(pic => pic.style.filter = `blur(${blur}px)`);
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            ticking = false;
+        });
+        ticking = true;
+    }
+});
   
 
 
